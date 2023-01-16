a1ceff6dc215a29d17d67a33ade72f6a42ae1934
Matti Räty <mattiraty@hotmail.com>
2023-01-16 11:44:10 +0200

    creating constructor for Field

--- Script/script.js
+++ Script/script.js
@@ -5,6 +5,13 @@ const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
 
+/**
+ * Field Class
+ */
+
 class Field {
-  
+    constructor(hatAndHoles, field) {
+        this._field = field;
+        this._hatAndHoles = hatAndHoles;
+    }
 }
